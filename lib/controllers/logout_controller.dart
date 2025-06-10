4c6da4936e482faaf7d0e5bcb321702c32275ff4
systemcrafter <edwin.mejia@outlook.com>
2025-06-09 21:28:01 -0600

    agregado print en logout

--- lib/controllers/logout_controller.dart
+++ lib/controllers/logout_controller.dart
@@ -1,9 +1,14 @@
+import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LogoutController {
   static Future<bool> logout() async {
     try {
       final prefs = await SharedPreferences.getInstance();
+
+      final token = prefs.getString('auth_token'); // clave correcta
+      debugPrint('Token logout: $token');
+
       await prefs.clear();
       return true;
     } catch (_) {
